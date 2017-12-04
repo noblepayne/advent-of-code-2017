6ff716d55442e45fcb2340e1f01076e6fb1788d9
Wes Payne <wpayne@marchex.com>
2017-12-03 20:30:15 -0800

    Sort of working

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [org.clojure/test.check "0.10.0-alpha2"]]
   :main ^:skip-aot advent-of-code-2017.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/advent_of_code_2017/core.clj
+++ src/advent_of_code_2017/core.clj
@@ -1,5 +1,9 @@
 (ns advent-of-code-2017.core
-  (:gen-class))
+  (:gen-class)
+  (:require ;[clojure.spec.alpha :as s]
+            ;[clojure.spec.gen.alpha :as gen]
+            ;[clojure.spec.test.alpha :as gen]
+            [advent-of-code-2017.day1.1 :as day1-1]))
 
 (defn -main
   "I don't do a whole lot ... yet."
