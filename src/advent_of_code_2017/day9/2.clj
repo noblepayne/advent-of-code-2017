4903c788dcf37e5f64b2d2bf7a7881dd1fcc8fad
Wes Payne <wpayne@marchex.com>
2017-12-09 16:34:46 -0800

    Clean up Day 9

--- src/advent_of_code_2017/day9/2.clj
+++ src/advent_of_code_2017/day9/2.clj
@@ -1,58 +1,43 @@
-(ns advent-of-code-2017.day9.1
+(ns advent-of-code-2017.day9.2
   (:require [clojure.core.match :refer [match]]
-             [clojure.spec.alpha :as s]))
+            [clojure.pprint :refer [pprint]]))
 
-(defn reducer [xs x]
-  (let [data (last xs)
-        [erase? garbage? cur-group group-count garbage-count] data]
-    (if erase?
-      (conj xs [false garbage? cur-group group-count garbage-count])
-      (if (= x \!)
-        (conj xs [true garbage? cur-group group-count garbage-count])
-        (if garbage?
-          (if (= x \>)
-            (conj xs [false false cur-group group-count garbage-count])
-            (conj xs [erase? garbage? cur-group group-count (inc garbage-count)]))
-          (condp = x
-            \<  (conj xs [false true cur-group group-count garbage-count])
-            \{  (conj xs [false false (inc cur-group) group-count garbage-count])
-            \}  (conj xs [false false (dec cur-group) (+ group-count cur-group) garbage-count])
-            (conj xs data)))))))
+(defn get-new-state [new-char state]
+  (match [new-char state]
+         [_  {::erase? true  ::garbage? true           ;; erase one character
+              ::depth _ ::count _ ::garbage-count _}]  (assoc state ::erase? false)
 
+         [\! {::erase? false ::garbage? true           ;; turn on erase
+              ::depth _ ::count _ ::garbage-count _}]  (assoc state ::erase? true)
 
+         [\> {::erase? false ::garbage? true           ;; close garbage
+              ::depth _ ::count _ ::garbage-count _}]  (assoc state ::garbage? false)
 
-(s/def ::accumulator (s/cat ::erase? boolean?
-                            ::garbage? boolean?
-                            ::depth int?
-                            ::count int?
-                            ::garbage-count int?))
+         [_  {::erase? false ::garbage? true           ;; increase garbage count
+              ::depth _ ::count _ ::garbage-count gc}] (assoc state ::garbage-count (inc gc))
 
-(defn better-reducer [xs x]
-  (let [state (s/conform ::accumulator (last xs))]
-    (->>
-     (match [x state]
-            [_  {::erase? true  ::garbage? _     ::depth _ ::count _ ::garbage-count _}]  (assoc state ::erase? false)
-            [\! {::erase? false ::garbage? _     ::depth _ ::count _ ::garbage-count _}]  (assoc state ::erase? true)
-            [\> {::erase? false ::garbage? true  ::depth _ ::count _ ::garbage-count _}]  (assoc state ::garbage? false)
-            [_  {::erase? false ::garbage? true  ::depth _ ::count _ ::garbage-count gc}] (assoc state ::garbage-count (inc gc))
-            [\< {::erase? false ::garbage? false ::depth _ ::count _ ::garbage-count _}]  (assoc state ::garbage? true)
-            [\{ {::erase? false ::garbage? false ::depth d ::count _ ::garbage-count _}]  (assoc state ::depth (inc d))
-            [\} {::erase? false ::garbage? false ::depth d ::count c ::garbage-count _}]  (assoc state
-                                                                                                 ::depth (dec d)
-                                                                                                 ::count (+ d c))
-            :else                                                                         state)
-     (s/unform ::accumulator)
-     (conj xs))))
+         [\< {::erase? false ::garbage? false          ;; start garbage
+              ::depth _ ::count _ ::garbage-count _}]  (assoc state ::garbage? true)
+
+         [\{ {::erase? false ::garbage? false          ;; new group
+              ::depth d ::count _ ::garbage-count _}]  (assoc state ::depth (inc d))
+
+         [\} {::erase? false ::garbage? false          ;; close group
+              ::depth d ::count c ::garbage-count _}]  (assoc state ::depth (dec d)
+                                                                    ::count (+ c d))
+         ;; ignore other characters
+         :else                                         state))
+
+(defn character-reducer [states new-char]
+  (->> states
+       last
+       (get-new-state new-char)
+       (conj states)))
 
 (defn solve-puzzle [puzzle-input]
   (->> puzzle-input
-       (reduce better-reducer [[false false 0 0 0]])
-       last))
-
-(def input "")
-
-(last
-      (last
-            (reduce reducer
-                    [[false false 0 0 0]]
-                    input)))
+       (reduce character-reducer
+               [{::erase? false ::garbage? false
+                 ::depth 0 ::count 0 ::garbage-count 0}])
+       last
+       pprint))
