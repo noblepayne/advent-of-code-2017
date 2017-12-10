215c2976dcb6e897c191e476431628e8190f9491
Wes Payne <wpayne@marchex.com>
2017-12-09 17:18:13 -0800

    Clean up Day 9

--- src/advent_of_code_2017/day9/2.clj
+++ src/advent_of_code_2017/day9/2.clj
@@ -4,29 +4,34 @@
 
 (defn get-new-state [new-char state]
   (match [new-char state]
-         [_  {::erase? true  ::garbage? true           ;; erase one character
-              ::depth _ ::count _ ::garbage-count _}]  (assoc state ::erase? false)
+    [_  {::erase?   true       ;; erase one character
+         ::garbage? true}]     (assoc state ::erase? false)
 
-         [\! {::erase? false ::garbage? true           ;; turn on erase
-              ::depth _ ::count _ ::garbage-count _}]  (assoc state ::erase? true)
+    [\! {::erase?   false      ;; turn on erase
+         ::garbage? true}]     (assoc state ::erase? true)
 
-         [\> {::erase? false ::garbage? true           ;; close garbage
-              ::depth _ ::count _ ::garbage-count _}]  (assoc state ::garbage? false)
+    [\> {::erase?   false      ;; close garbage
+         ::garbage? true}]     (assoc state ::garbage? false)
 
-         [_  {::erase? false ::garbage? true           ;; increase garbage count
-              ::depth _ ::count _ ::garbage-count gc}] (assoc state ::garbage-count (inc gc))
+    [_  {::erase?   false
+         ::garbage? true       ;; increase garbage count
+         ::garbage-count gc}]  (assoc state ::garbage-count (inc gc))
 
-         [\< {::erase? false ::garbage? false          ;; start garbage
-              ::depth _ ::count _ ::garbage-count _}]  (assoc state ::garbage? true)
+    [\< {::erase?   false      ;; start garbage
+         ::garbage? false}]    (assoc state ::garbage? true)
 
-         [\{ {::erase? false ::garbage? false          ;; new group
-              ::depth d ::count _ ::garbage-count _}]  (assoc state ::depth (inc d))
+    [\{ {::erase?   false
+         ::garbage? false      ;; new group
+         ::depth    d}]        (assoc state ::depth (inc d))
 
-         [\} {::erase? false ::garbage? false          ;; close group
-              ::depth d ::count c ::garbage-count _}]  (assoc state ::depth (dec d)
-                                                                    ::count (+ c d))
-         ;; ignore other characters
-         :else                                         state))
+    [\} {::erase?   false
+         ::garbage? false
+         ::depth    d          ;; close group
+         ::count    c}]        (assoc state ::depth (dec d)
+                                            ::count (+ c d))
+
+                               ;; ignore other characters
+    :else                      state))
 
 (defn character-reducer [states new-char]
   (->> states
